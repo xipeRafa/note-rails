6027cd2004e7019b3417f23edb851cdfd925b004
Rafa <rafa@pop-os.localdomain>
2018-10-26 16:48:27 -0600

    relation user id

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_213350) do
+ActiveRecord::Schema.define(version: 2018_10_26_222341) do
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
